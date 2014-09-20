c00af6eadd73160278a6951f8b8fdfd2bc056aa4
olek007 <kelor2@o2.pl>
2014-09-20 18:55:46 +0200

    heheheheheheheh
    asdasdasdasdasdasd

---
+++ Assets/Scripts/exit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exit : MonoBehaviour {
+	
+	void Update () {	
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+	public Texture tekstura;
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			Application.LoadLevel (1);
+		}
+	}
+		
+	void OnGUI ()
+	{
+		GUI.DrawTexture(new Rect(Screen.width/2 - tekstura.width/2,Screen.height/2 - tekstura.height/2,tekstura.width,tekstura.height), tekstura);
+	}
+	
+}
