03b8a126a5a9329afc457da896426deffbd967ac
olek007 <kelor2@o2.pl>
2014-09-20 10:03:16 +0200

    chodzenie
    IT WORKING!!!

---
+++ Assets/Postac.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Postac : MonoBehaviour {
+
+	NavMeshAgent agent;
+	Vector3 mouseSkrin;
+	RaycastHit hit;
+	public Camera kamera;
+	Ray dest;
+
+	void Start()
+	{
+		agent=GetComponent<NavMeshAgent>();
+	}
+
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			dest = kamera.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(dest,out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
